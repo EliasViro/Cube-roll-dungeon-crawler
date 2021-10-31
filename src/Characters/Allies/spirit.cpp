84e34c5f40984daef44c0e556ac195844307ef64
viroe1 <elias.viro@aalto.fi>
2021-10-31 12:19:34 +0200

    renaming some files

--- src/Characters/Allies/spirit.cpp
+++ src/Characters/Allies/spirit.cpp
@@ -2,9 +2,9 @@
 
 
 //A basic ally that moves one tile towards the nearest enemy every turn.
-//If it can't proceed towards its target due to having a wall or a pit 
+//If it can't proceed towards its target due to having a wall 
 //between it and the target, it will move to a direction that will have
 //the shortest route to its target. Spirits will die when there are no
 //enemies in a room. The spirit will also switch to the nearest target if
-//its current target is further than another enemy.
+//its current target is further than another enemy. Flying.
 //Action pattern: Attack(2)

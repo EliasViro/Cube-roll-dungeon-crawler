c9f1ccbd4647adf6805e2dcb9a9341a6b8a8434b
viroe1 <elias.viro@aalto.fi>
2021-10-31 12:20:45 +0200

    commit

--- src/DungeonGeneration/DungeonTiles/pittile.hpp
+++
@@ -1,8 +0,0 @@
-
-
-
-//A pit tile, represented by 'P' in room files.
-//Unpassable, but doesn't prevent ranged attacks through it. 
-//Deals 999 damage to any character that moves (is pushed) onto it.
-//flying enemies are an exception and can move on pit tiles and
-//will not take damage from the tile.

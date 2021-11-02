7ab5ba9351a8bf360e43bdc8cceaaaaf7505e897
viroe1 <elias.viro@aalto.fi>
2021-11-02 09:29:34 +0200

    Added the Item superclass.

--- src/Inventory/inventoryslot.cpp
+++ src/Inventory/inventoryslot.cpp
@@ -1,3 +1,3 @@
-
+#include "inventoryslot.hpp"
 
 //A class that represents one of the six slots in the player inventory. Holds an Item.
--- src/Inventory/inventoryslot.hpp
+++ src/Inventory/inventoryslot.hpp
@@ -1,3 +1,3 @@
-
+#include "src/Items/item.hpp"
 
 //A class that represents one of the six slots in the player inventory. Holds an Item.

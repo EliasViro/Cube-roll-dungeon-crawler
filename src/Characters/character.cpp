c32c3275167adee91a087c74b738029a3a175bbe
viroe1 <elias.viro@aalto.fi>
2021-11-02 11:47:37 +0200

    Created Character, DungeonTile and InventorySlot.

--- src/Characters/character.cpp
+++ src/Characters/character.cpp
@@ -1,7 +1,27 @@
-
+#include "character.hpp"
 
 
 //Character is a class that all character-related classes inherit.
 //A character represents an active entity in a room, such as the player, 
 //enemies or allied characters. Holds information such as the type of
-//the character (player, hostile, allied) and its health points.
+//the character (player, enemy, allied) and its health points.
+
+Character::Character(CharacterType charactertype, int healthpoints)
+    : charactertype_(charactertype), healthpoints_(healthpoints), defensepoints_(0) {}
+
+Character::~Character() {}
+
+CharacterType Character::GetCharacterType() const {
+    return charactertype_;
+}
+
+int Character::GetHealthPoints() const {
+    return healthpoints_;
+}
+
+void Character::TakeDamage(int damage) {
+    healthpoints_ = healthpoints_ - (damage - defensepoints_);
+    if (healthpoints_ <= 0) {
+        delete(this);
+    }
+}
--- src/Characters/character.hpp
+++ src/Characters/character.hpp
@@ -4,4 +4,35 @@
 //Character is a class that all character-related classes inherit.
 //A character represents an active entity in a room, such as the player, 
 //enemies or allied characters. Holds information such as the type of
-//the character (player, hostile, allied) and its health points.
+//the character (player, enemy, allied) and its health points.
+
+enum CharacterType {
+    Player,
+    Allied,
+    Enemy
+};
+
+class Character {
+    public:
+    Character(CharacterType charactertype, int healthpoints); //Constructs a new character.
+
+    ~Character(); //Destructor that destroys the character if health points
+    //are reduced to zero or below.
+
+    CharacterType GetCharacterType() const;
+
+    int GetHealthPoints() const;
+
+    void TakeDamage(int damage);
+
+    virtual void TakeAction() = 0; //Overridden by Allied and Enemy subclasses.
+    //Causes Allies and Enemies to take an action.
+
+    virtual const char PathFind() = 0; //Overridden by Allied and Enemy subclasses.
+    //Returns the direction the character wants to move next as a character of "NEWS".
+
+    protected:
+    CharacterType charactertype_; //The type of the character.
+    int healthpoints_; //The health points of the character.
+    int defensepoints_; //The defense points of the character from using a shield.
+};

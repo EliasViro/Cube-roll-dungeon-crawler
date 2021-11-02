f8f96c813591913073bd6a4e63a1260b03aba71c
Atte <aarnioa6@aalto.fi>
2021-11-02 20:59:38 +0200

    CMake test: adds all subdirs of src succesfully.

---
+++ src/Characters/Allies/ATTE_CMakeTest.hpp
@@ -0,0 +1,6 @@
+#include <string>
+
+std::string dummyfunction() {
+    std::string str = "Well hello there!";
+    return str;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
+#include "Characters/Allies/ATTE_CMakeTest.hpp"
 
 int main() {
     std::cout << "Hello world!" << std::endl;
+    std::string str = dummyfunction();
+    std::cout << str << std::endl;
     return 0;
 }

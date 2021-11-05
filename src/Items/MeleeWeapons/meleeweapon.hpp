f1a33a750be0a3c9699d80065c28a357de31d8a9
taskins4 <selin.taskin@aalto.fi>
2021-11-05 13:20:40 +0200

    melee weapon class

--- src/Items/MeleeWeapons/meleeweapon.hpp
+++ src/Items/MeleeWeapons/meleeweapon.hpp
@@ -5,3 +5,27 @@
 //The conditions for using a melee weapon:
 //-An enemy is in range of the weapon
 //-The weapon isn't on cooldown.
+#include <stdio.h>
+#include "items.hpp"
+#ifndef meleeweapon_hpp
+#define meleeweapon_hpp
+
+
+
+class Meleeweapon: public Item {
+public:
+    Meleeweapon (const std::string& name, unsigned int durability, unsigned int maxcooldown, int range):Item(name,MeleeWeapon, durability, maxcooldown){
+        range_=range;
+    }
+    
+    bool CanBeUsed() const;
+    
+    void Trigger();
+    
+    std::string &GetDescription() const;
+protected:
+    int range_;
+    
+};
+
+#endif /* meleeweapon_hpp */

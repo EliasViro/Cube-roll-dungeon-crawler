0a913c5c5f9f77c1ffc36c7bfe0b7ffc757f3be0
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:35:46 +0200

    dagger hpp implementation

--- src/Items/MeleeWeapons/dagger.hpp
+++ src/Items/MeleeWeapons/dagger.hpp
@@ -3,3 +3,17 @@
 
 //A basic melee weapon with improved attack speed.
 //Range - 1, damage - 1, max cooldown - 3, durability - 999
+#include "items.hpp"
+#include "meleeweapon.hpp"
+#ifndef dagger_hpp
+#define dagger_hpp
+
+#include <stdio.h>
+class Dagger:public Meleeweapon{
+public:
+    Dagger ();
+   
+    };
+
+
+#endif /* dagger_hpp */

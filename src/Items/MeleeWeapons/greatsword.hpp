26efe69007b86c08024e63322c22a45880cc9160
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:36:58 +0200

    greatsword hpp implementation

--- src/Items/MeleeWeapons/greatsword.hpp
+++ src/Items/MeleeWeapons/greatsword.hpp
@@ -3,3 +3,17 @@
 
 //A powerful melee weapon that hits all enemies in the attack range.
 //Range - 1, damage - 2, max cooldown - 6, durability - 999
+#include "items.hpp"
+#include "meleeweapon.hpp"
+#ifndef greatsword_hpp
+#define greatsword_hpp
+
+#include <stdio.h>
+class GreatSword:public Meleeweapon{
+public:
+    GreatSword();
+    
+    };
+
+
+#endif /* greatsword_hpp */

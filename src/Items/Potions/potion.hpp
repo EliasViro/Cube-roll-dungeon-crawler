cdbcca5ac7090144fc4775da880866e7fce411ae
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:42:21 +0200

    potion hpp implementation

--- src/Items/Potions/potion.hpp
+++ src/Items/Potions/potion.hpp
@@ -3,3 +3,19 @@
 
 //A class that all potions inherit.
 //Potions have only one point of durability.
+#include "items.hpp"
+#ifndef potion_hpp
+#define potion_hpp
+
+#include <stdio.h>
+
+class Potions: public Item {
+public:
+    Potions(const std::string& name );
+   
+protected:
+    bool used_;
+    std::string name_;
+};
+
+#endif /* potion_hpp */

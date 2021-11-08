4075fc840224553de997b4623a1163ece134a98c
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:38:25 +0200

    longsword hpp implementation

--- src/Items/MeleeWeapons/longsword.hpp
+++ src/Items/MeleeWeapons/longsword.hpp
@@ -3,3 +3,20 @@
 
 //A melee weapon.
 //Range - 1, damage - 2, max cooldown - 5, durability - 999
+#include "items.hpp"
+#include "meleeweapon.hpp"
+#ifndef longsword_hpp
+#define longsword_hpp
+
+#include <stdio.h>
+
+
+
+class LongSword:public Meleeweapon{
+public:
+    LongSword();
+    
+    };
+
+
+#endif /* longsword_hpp */

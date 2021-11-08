4b789729f17ef30a77853d716ac36ed29ab24bb8
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:52:25 +0200

    health potion hpp implementation

--- src/Items/Potions/healthpotion.hpp
+++ src/Items/Potions/healthpotion.hpp
@@ -3,3 +3,19 @@
 
 //Restores one health point to the player.
 //Using condition: The player has missing health points.
+#include "potion.hpp"
+#ifndef healthpotion_hpp
+#define healthpotion_hpp
+
+#include <stdio.h>
+class HealthPotion:public Potions{
+public:
+    HealthPotion();
+    bool CanBeUsed() const ;
+    
+    void Trigger();
+    
+    std::string GetDescription() const;
+    
+};
+#endif /* healthpotion_hpp */

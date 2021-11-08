7d1b285f9ae6a049a5692275dc3d74d9ae551671
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:50:30 +0200

    hastepotion hpp implementation

--- src/Items/Potions/hastepotion.hpp
+++ src/Items/Potions/hastepotion.hpp
@@ -4,3 +4,20 @@
 //A potion that allows the player to take two turns instead of one
 //immediately after drinking the potion.
 //Using condition: There are enemies in the same room as the player.
+#include "potion.hpp"
+#ifndef hastepotion_hpp
+#define hastepotion_hpp
+
+#include <stdio.h>
+class HastePotion:public Potions{
+public:
+    HastePotion();
+    bool CanBeUsed() const ;
+    
+    void Trigger();
+    
+    std::string GetDescription() const;
+    
+};
+
+#endif /* hastepotion_hpp */

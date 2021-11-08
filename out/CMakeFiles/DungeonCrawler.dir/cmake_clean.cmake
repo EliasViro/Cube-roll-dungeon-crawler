ff0005de66ec9f89e3ae8993c04e53a2acd58949
Atte <aarnioa6@aalto.fi>
2021-11-08 16:09:11 +0200

    Succesful CMaek + SFML test on my system.

---
+++ out/CMakeFiles/DungeonCrawler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DungeonCrawler.dir/src/main.cpp.o"
+  "DungeonCrawler"
+  "DungeonCrawler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DungeonCrawler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
 #include "Characters/Allies/ATTE_CMakeTest.hpp"
+#include <SFML/Graphics.hpp>
 
 int main() {
     std::cout << "Hello world!" << std::endl;
     std::string str = dummyfunction();
     std::cout << str << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }

986da8507d8eccc05e1480a71e681e83287e42b7
taskins4 <selin.taskin@aalto.fi>
2021-11-08 04:35:19 +0200

    axe cpp implementation

--- src/Items/MeleeWeapons/axe.cpp
+++ src/Items/MeleeWeapons/axe.cpp
@@ -3,3 +3,10 @@
 
 //A melee weapon.
 //Range - 1, damage - 2, max cooldown - 6, durability - 999
+#include "meleeweapon.hpp"
+#include "axe.hpp"
+Axe::Axe():Meleeweapon("Axe",999, 6,2){
+    cooldown_=0;
+    isactive_=false;
+    
+};

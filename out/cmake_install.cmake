3f0d3352a05f20fa11050e6f20ac4935923261ad
Atte <aarnioa6@aalto.fi>
2021-11-10 10:54:52 +0200

    Game window skeleton + test instructions.

--- out/cmake_install.cmake
+++ out/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/Elias Viro/Documents/cpp-autumn2021/Project/dungeon-crawler-group-3
+# Install script for directory: /home/atte/cpp_projects/DungeonCrawler
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/mnt/c/Users/Elias Viro/Documents/cpp-autumn2021/Project/dungeon-crawler-group-3/out/src/cmake_install.cmake")
+  include("/home/atte/cpp_projects/DungeonCrawler/out/src/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/Elias Viro/Documents/cpp-autumn2021/Project/dungeon-crawler-group-3/out/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/atte/cpp_projects/DungeonCrawler/out/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

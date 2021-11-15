0b5f2583962a439692cd0882c7d707abb705d253
viroe1 <elias.viro@aalto.fi>
2021-11-15 16:52:53 +0200

    Test

--- tests/dungeonleveltest.cpp
+++ tests/dungeonleveltest.cpp
@@ -1,9 +1,6 @@
-#include "../src/DungeonGeneration/dungeonroom.hpp"
+#include "../src/DungeonGeneration/dungeontile.hpp"
 #include <utility>
 #include <iostream>
 
-std::pair<int,int> joku = std::make_pair(1,1);
-
-auto lol = DungeonRoom(joku, 1, _1DoorRoom, North, nullptr, false);
-
-std::cout << lol.GetIndexInLevel() << " <-- this should be 1."<<std::endl;
+auto joku = DungeonTile(TileType::Wall, 1, 2);
+std::cout << joku.GetXCoord() << " <-- this should be 1." << std::endl;

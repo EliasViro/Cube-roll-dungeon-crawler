862c8ad33ea0e04efbfe9d7d27314933cf4aa24f
Atte <aarnioa6@aalto.fi>
2021-11-15 15:46:36 +0200

    Start menu buttons and signals done.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,43 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Audio.hpp>
 
+// User-specific local paths
+//############################
+// Go into the directory Graphcis/fonts. 
+// Install the fff-forward-regular.ttf font on your local system.
+// Copy the full path of the .ttf in the installation directory and save it into the following variable.
+std::string fff_forward_path = "/home/atte/.local/share/fonts/Unknown Vendor/TrueType/FFF Forward/FFF_Forward_Regular.ttf";
+//############################
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "Dungeon Crawler");
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "Dungeon Crawler", sf::Style::Close);
     
     sf::RectangleShape quit_button(sf::Vector2f(300, 100));
-    quit_button.setOrigin(150, 50);
+    quit_button.setOrigin(quit_button.getLocalBounds().width/2, quit_button.getLocalBounds().height/2);
     quit_button.setPosition(800, 600);
     quit_button.setFillColor(sf::Color::Magenta);
     
     sf::RectangleShape start_button(sf::Vector2f(360, 120));
-    start_button.setOrigin(180, 60);
+    start_button.setOrigin(start_button.getLocalBounds().width/2, start_button.getLocalBounds().height/2);
     start_button.setPosition(800, 300);
     start_button.setFillColor(sf::Color::Cyan);
 
     sf::Font pixel_font;
-    pixel_font.loadFromFile("Graphics/fonts/fff-forward-regular.ttf");
+    pixel_font.loadFromFile(fff_forward_path);
 
     sf::Text quit_text("Quit", pixel_font, 30);
-    quit_text.setOrigin(quit_text.getLocalBounds().width/2, quit_text.getLocalBounds().height/2);
+    quit_text.setOrigin(quit_text.getLocalBounds().width/2, quit_text.getLocalBounds().height/2 - 10);
     quit_text.setPosition(quit_button.getPosition());
+
+    sf::Text start_text("Start game", pixel_font, 40);
+    start_text.setOrigin(start_text.getLocalBounds().width/2, start_text.getLocalBounds().height/2 - 10);
+    start_text.setPosition(start_button.getPosition());
+
+    sf::Music start_music;
+    start_music.openFromFile("src/Sounds/Placeholder.mp3");
 
     while (window.isOpen())
     {
@@ -30,22 +48,27 @@ int main() {
                 window.close();
         }
 
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+            if (quit_button.getGlobalBounds().contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y))) {
+                window.close();
+            }
+            else if (start_button.getGlobalBounds().contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y))) {
+                std::cout << "Start button works" << std::endl;
+            }
+
+        }
+
         window.clear();
         window.draw(quit_button);
         window.draw(start_button);
         window.draw(quit_text);
+        window.draw(start_text);
         window.display();
+
+        //start_music.play();
     }
 
     return 0;
 
 }
-
-
-/*
-
-
-sf::CircleShape shape(100.f);
-shape.setFillColor(sf::Color::Green);
-
-*/

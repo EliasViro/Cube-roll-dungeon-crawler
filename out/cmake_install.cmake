dd9a759e745340fd95c1096fd2796ed3f9ed216f
viroe1 <elias.viro@aalto.fi>
2021-11-22 20:00:24 +0200

    Test

--- out/cmake_install.cmake
+++ out/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/Elias Viro/Documents/cpp-autumn2021/Project/dungeon-crawler-group-3/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/Elias Viro/Documents/cpp-autumn2021/Project/dungeon-crawler-group-3/out/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/TEST_Atte.cpp
+++ src/TEST_Atte.cpp
@@ -1,6 +1,8 @@
+/*
 #include "DungeonGeneration/dungeonlevel.hpp"
 
 int main() {
     auto level = new DungeonLevel(3);
     return 0;
 }
+*/

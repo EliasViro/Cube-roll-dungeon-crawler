1024f15ef7cd705999045b8ba02b48e97c00d6c1
Atte <aarnioa6@aalto.fi>
2021-11-26 17:45:51 +0200

    New textures + textures class cleaned

--- src/textures.cpp
+++ src/textures.cpp
@@ -3,320 +3,85 @@
 class Textures {
 public:
 	Textures() {
-		//sf::Texture game_texture;
-    	//game_texture.loadFromFile("../src/Graphics/GUI_Sprites/UI_PIC.png");
-		//game_view = game_texture;
-
 		game_view.loadFromFile("../src/Graphics/GUI_Sprites/UI_PIC.png");
-
-		sf::Texture door_closed_t;
-		door_closed_t.loadFromFile("../src/Graphics/TileSprites/DoorClosed.png");
-		door_closed = door_closed_t;
-
-		sf::Texture floor_t;
-		floor_t.loadFromFile("../src/Graphics/TileSprites/Floor.png");
-		floor = floor_t;
-
-		sf::Texture levelexitclosed_t;
-		levelexitclosed_t.loadFromFile("../src/Graphics/TileSprites/LevelExitClosed.png");
-		levelexitclosed = levelexitclosed_t;
-
-		sf::Texture levelexitopen_t;
-		levelexitopen_t.loadFromFile("../src/Graphics/TileSprites/LevelExitOpen.png");
-		levelexitopen = levelexitopen_t;
-
-		sf::Texture pit_t;
-		pit_t.loadFromFile("../src/Graphics/TileSprites/Pit.png");
-		pit = pit_t;
-
-		sf::Texture trap1_t;
-		trap1_t.loadFromFile("../src/Graphics/TileSprites/Trap1.png");
-		trap1 = trap1_t;
-
-		sf::Texture trap2_t;
-		trap2_t.loadFromFile("../src/Graphics/TileSprites/Trap2.png");
-		trap2 = trap2_t;
-
-		sf::Texture trap3_t;
-		trap3_t.loadFromFile("../src/Graphics/TileSprites/Trap3.png");
-		trap3 = trap3_t;
-
-		sf::Texture wall_t;
-		wall_t.loadFromFile("../src/Graphics/TileSprites/Wall.png");
-		wall = wall_t;
-
-		sf::Texture healthpotion_t;
-		healthpotion_t.loadFromFile("../src/Graphics/ItemSprites/HealthPotion.png");
-		healthpotion = healthpotion_t;
-
-		sf::Texture staminapotion_t;
-		staminapotion_t.loadFromFile("../src/Graphics/ItemSprites/StaminaPotion.png");
-		staminapotion = staminapotion_t;
-
-		sf::Texture shortsword_t;
-		shortsword_t.loadFromFile("../src/Graphics/ItemSprites/ShortSword.png");
-		shortsword = shortsword_t;
-
-		sf::Texture armingsword_t;
-		armingsword_t.loadFromFile("../src/Graphics/ItemSprites/ArmingSword.png");
-		armingsword = armingsword_t;
-
-		sf::Texture longsword_t;
-		longsword_t.loadFromFile("../src/Graphics/ItemSprites/LongSword.png");
-		longsword = longsword_t;
-
-		sf::Texture hatchet_t;
-		hatchet_t.loadFromFile("../src/Graphics/ItemSprites/Hatchet.png");
-		hatchet = hatchet_t;
-
-		sf::Texture battleaxe_t;
-		battleaxe_t.loadFromFile("../src/Graphics/ItemSprites/BattleAxe.png");
-		battleaxe = battleaxe_t;
-
-		sf::Texture mace_t;
-		mace_t.loadFromFile("../src/Graphics/ItemSprites/Mace.png");
-		mace = mace_t;
-
-		sf::Texture warhammer_t;
-		warhammer_t.loadFromFile("../src/Graphics/ItemSprites/WarHammer.png");
-		warhammer = warhammer_t;
-
-		sf::Texture roundshield_t;
-		roundshield_t.loadFromFile("../src/Graphics/ItemSprites/RoundShield.png");
-		roundshield = roundshield_t;
-
-		sf::Texture heatershield_t;
-		heatershield_t.loadFromFile("../src/Graphics/ItemSprites/HeaterShield.png");
-		heatershield = heatershield_t;
-
-		sf::Texture kiteshield_t;
-		kiteshield_t.loadFromFile("../src/Graphics/ItemSprites/KiteShield.png");
-		kiteshield = kiteshield_t;
-
-		sf::Texture towershield_t;
-		towershield_t.loadFromFile("../src/Graphics/ItemSprites/TowerShield.png");
-		towershield = towershield_t;
-
-		sf::Texture sling_t;
-		sling_t.loadFromFile("../src/Graphics/ItemSprites/Sling.png");
-		sling = sling_t;
-
-		sf::Texture bolas_t;
-		bolas_t.loadFromFile("../src/Graphics/ItemSprites/Bolas.png");
-		bolas = bolas_t;
-
-		sf::Texture javelin_t;
-		javelin_t.loadFromFile("../src/Graphics/ItemSprites/Javelin.png");
-		javelin = javelin_t;
-
-		sf::Texture aiaggressive_t;
-		aiaggressive_t.loadFromFile("../src/Graphics/CharacterSprites/AI_Aggressive.png");
-		aiaggressive = aiaggressive_t;
-
-		sf::Texture aicareful_t;
-		aicareful_t.loadFromFile("../src/Graphics/CharacterSprites/AI_Careful.png");
-		aicareful = aicareful_t;
-
-		sf::Texture aiboss_t;
-		aiboss_t.loadFromFile("../src/Graphics/CharacterSprites/AI_Boss.png");
-		aiboss = aiboss_t;
-
-		sf::Texture airandom_t;
-		airandom_t.loadFromFile("../src/Graphics/CharacterSprites/AI_Random.png");
-		airandom = airandom_t;
-
-		sf::Texture aistunned_t;
-		aistunned_t.loadFromFile("../src/Graphics/CharacterSprites/AI_Stunned.png");
-		aistunned = aistunned_t;
-
-		sf::Texture defense1_t;
-		defense1_t.loadFromFile("../src/Graphics/CharacterSprites/Defense1.png");
-		defense1 = defense1_t;
-
-		sf::Texture defense2_t;
-		defense2_t.loadFromFile("../src/Graphics/CharacterSprites/Defense2.png");
-		defense2 = defense2_t;
-
-		sf::Texture defense3_t;
-		defense3_t.loadFromFile("../src/Graphics/CharacterSprites/Defense3.png");
-		defense3 = defense3_t;
-
-		sf::Texture melee1_t;
-		melee1_t.loadFromFile("../src/Graphics/CharacterSprites/Melee1.png");
-		melee1 = melee1_t;
-
-		sf::Texture melee2_t;
-		melee2_t.loadFromFile("../src/Graphics/CharacterSprites/Melee2.png");
-		melee2 = melee2_t;
-
-		sf::Texture melee3_t;
-		melee3_t.loadFromFile("../src/Graphics/CharacterSprites/Melee3.png");
-		melee3 = melee3_t;
-
-		sf::Texture ranged1_t;
-		ranged1_t.loadFromFile("../src/Graphics/CharacterSprites/Ranged1.png");
-		ranged1 = ranged1_t;
-
-		sf::Texture ranged2_t;
-		ranged2_t.loadFromFile("../src/Graphics/CharacterSprites/Ranged2.png");
-		ranged2 = ranged2_t;
-
-		sf::Texture ranged3_t;
-		ranged3_t.loadFromFile("../src/Graphics/CharacterSprites/Ranged3.png");
-		ranged3 = ranged3_t;
-
-		sf::Texture emptyaction_t;
-		emptyaction_t.loadFromFile("../src/Graphics/CharacterSprites/Empty.png");
-		emptyaction = emptyaction_t;
-
-		sf::Texture enemybase_t;
-		enemybase_t.loadFromFile("../src/Graphics/CharacterSprites/Enemy.png");
-		enemybase = enemybase_t;
-
-		sf::Texture playerbase_t;
-		playerbase_t.loadFromFile("../src/Graphics/CharacterSprites/Player.png");
-		playerbase = playerbase_t;
-
-		sf::Texture depth1_t;
-		depth1_t.loadFromFile("../src/Graphics/GUI_Sprites/Depth1.png");
-		depth1 = depth1_t;
-
-		sf::Texture depth2_t;
-		depth2_t.loadFromFile("../src/Graphics/GUI_Sprites/Depth2.png");
-		depth2 = depth2_t;
-
-		sf::Texture depth3_t;
-		depth3_t.loadFromFile("../src/Graphics/GUI_Sprites/Depth3.png");
-		depth3 = depth3_t;
-
-		sf::Texture depth4_t;
-		depth4_t.loadFromFile("../src/Graphics/GUI_Sprites/Depth4.png");
-		depth4 = depth4_t;
-
-		sf::Texture depth5_t;
-		depth5_t.loadFromFile("../src/Graphics/GUI_Sprites/Depth5.png");
-		depth5 = depth5_t;
-
-		sf::Texture depth6_t;
-		depth6_t.loadFromFile("../src/Graphics/GUI_Sprites/Depth6.png");
-		depth6 = depth6_t;
-
-		sf::Texture health0_t;
-		health0_t.loadFromFile("../src/Graphics/GUI_Sprites/Health0.png");
-		health0 = health0_t;
-
-		sf::Texture health1_t;
-		health1_t.loadFromFile("../src/Graphics/GUI_Sprites/Health1.png");
-		health1 = health1_t;
-
-		sf::Texture health2_t;
-		health2_t.loadFromFile("../src/Graphics/GUI_Sprites/Health2.png");
-		health2 = health2_t;
-
-		sf::Texture health3_t;
-		health3_t.loadFromFile("../src/Graphics/GUI_Sprites/Health3.png");
-		health3 = health3_t;
-		
-		sf::Texture health4_t;
-		health4_t.loadFromFile("../src/Graphics/GUI_Sprites/Health4.png");
-		health4 = health4_t;
-
-		sf::Texture nrzero_t;
-		nrzero_t.loadFromFile("../src/Graphics/GUI_Sprites/0.png");
-		nrzero = nrzero_t;
-
-		sf::Texture nrone_t;
-		nrone_t.loadFromFile("../src/Graphics/GUI_Sprites/1.png");
-		nrone = nrone_t;
-
-		sf::Texture nrtwo_t;
-		nrtwo_t.loadFromFile("../src/Graphics/GUI_Sprites/2.png");
-		nrtwo = nrtwo_t;
-
-		sf::Texture nrthree_t;
-		nrthree_t.loadFromFile("../src/Graphics/GUI_Sprites/3.png");
-		nrthree = nrthree_t;
-
-		sf::Texture nrfour_t;
-		nrfour_t.loadFromFile("../src/Graphics/GUI_Sprites/4.png");
-		nrfour = nrfour_t;
-
-		sf::Texture nrfive_t;
-		nrfive_t.loadFromFile("../src/Graphics/GUI_Sprites/5.png");
-		nrfive = nrfive_t;
-
-		sf::Texture nrsix_t;
-		nrsix_t.loadFromFile("../src/Graphics/GUI_Sprites/6.png");
-		nrsix = nrsix_t;
-
-		sf::Texture nrseven_t;
-		nrseven_t.loadFromFile("../src/Graphics/GUI_Sprites/7.png");
-		nrseven = nrseven_t;
-
-		sf::Texture nreight_t;
-		nreight_t.loadFromFile("../src/Graphics/GUI_Sprites/8.png");
-		nreight = nreight_t;
-
-		sf::Texture nrinf_t;
-		nrinf_t.loadFromFile("../src/Graphics/GUI_Sprites/Inf.png");
-		nrinf = nrinf_t;
-
-		sf::Texture att_t;
-		att_t.loadFromFile("../src/Graphics/GUI_Sprites/Att.png");
-		att = att_t;
-
-		sf::Texture defse_t;
-		defse_t.loadFromFile("../src/Graphics/GUI_Sprites/Def.png");
-		defse = defse_t;
-
-		sf::Texture coold_t;
-		coold_t.loadFromFile("../src/Graphics/GUI_Sprites/CD.png");
-		coold = coold_t;
-
-		sf::Texture maxcoold_t;
-		maxcoold_t.loadFromFile("../src/Graphics/GUI_Sprites/TCD.png");
-		maxcoold = maxcoold_t;
-
-		sf::Texture dur_t;
-		dur_t.loadFromFile("../src/Graphics/GUI_Sprites/Dur.png");
-		dur = dur_t;
-
-		sf::Texture nonum_t;
-		nonum_t.loadFromFile("../src/Graphics/GUI_Sprites/NoNum.png");
-		nonum = nonum_t;
-
-		sf::Texture infobtnview_t;
-		infobtnview_t.loadFromFile("../src/Graphics/GUI_Sprites/infobtnview.png");
-		infobtnview = infobtnview_t;
-
-		sf::Texture diminvbutton_t;
-		diminvbutton_t.loadFromFile("../src/Graphics/GUI_Sprites/DimInvButton.png");
-		diminvbutton = diminvbutton_t;
-
-		sf::Texture hugeslimeinfo_t;
-		hugeslimeinfo_t.loadFromFile("../src/Graphics/CharacterInfoSprites/HugeSlimeInfo.png");
-		hugespiderinfo = hugeslimeinfo_t;
-
-		sf::Texture hugespiderinfo_t;
-		hugespiderinfo_t.loadFromFile("../src/Graphics/CharacterInfoSprites/HugeSpiderInfo.png");
-		hugespiderinfo = hugespiderinfo_t;
-
-		sf::Texture largeslimeinfo_t;
-		largeslimeinfo_t.loadFromFile("../src/Graphics/CharacterInfoSprites/LargeSlimeInfo.png");
-		largeslimeinfo = largeslimeinfo_t;
-
-		sf::Texture lichinfo_t;
-		lichinfo_t.loadFromFile("../src/Graphics/CharacterInfoSprites/LichInfo.png");
-		lichinfo = lichinfo_t;
-
-		sf::Texture selectionrectangle_t;
-		selectionrectangle_t.loadFromFile("../src/Graphics/CharacterInfoSprites/SelectionRectangle.png");
-		selectionrectangle = selectionrectangle_t;
-
-		sf::Texture skeletonarcherinfo_t;
-		skeletonarcherinfo_t.loadFromFile("../src/Graphics/CharacterInfoSprites/SkeletonArcherInfo.png");
+		door_closed.loadFromFile("../src/Graphics/TileSprites/DoorClosed.png");
+		floor.loadFromFile("../src/Graphics/TileSprites/Floor.png");
+		levelexitclosed.loadFromFile("../src/Graphics/TileSprites/LevelExitClosed.png");
+		levelexitopen.loadFromFile("../src/Graphics/TileSprites/LevelExitOpen.png");
+		pit.loadFromFile("../src/Graphics/TileSprites/Pit.png");
+		trap1.loadFromFile("../src/Graphics/TileSprites/Trap1.png");
+		trap2.loadFromFile("../src/Graphics/TileSprites/Trap2.png");
+		trap3.loadFromFile("../src/Graphics/TileSprites/Trap3.png");
+		wall.loadFromFile("../src/Graphics/TileSprites/Wall.png");
+		healthpotion.loadFromFile("../src/Graphics/ItemSprites/HealthPotion.png");
+		staminapotion.loadFromFile("../src/Graphics/ItemSprites/StaminaPotion.png");
+		shortsword.loadFromFile("../src/Graphics/ItemSprites/ShortSword.png");
+		armingsword.loadFromFile("../src/Graphics/ItemSprites/ArmingSword.png");
+		longsword.loadFromFile("../src/Graphics/ItemSprites/LongSword.png");
+		hatchet.loadFromFile("../src/Graphics/ItemSprites/Hatchet.png");
+		battleaxe.loadFromFile("../src/Graphics/ItemSprites/BattleAxe.png");
+		mace.loadFromFile("../src/Graphics/ItemSprites/Mace.png");
+		warhammer.loadFromFile("../src/Graphics/ItemSprites/WarHammer.png");
+		roundshield.loadFromFile("../src/Graphics/ItemSprites/RoundShield.png");
+		heatershield.loadFromFile("../src/Graphics/ItemSprites/HeaterShield.png");
+		kiteshield.loadFromFile("../src/Graphics/ItemSprites/KiteShield.png");
+		towershield.loadFromFile("../src/Graphics/ItemSprites/TowerShield.png");
+		sling.loadFromFile("../src/Graphics/ItemSprites/Sling.png");
+		bolas.loadFromFile("../src/Graphics/ItemSprites/Bolas.png");
+		javelin.loadFromFile("../src/Graphics/ItemSprites/Javelin.png");
+		aiaggressive.loadFromFile("../src/Graphics/CharacterSprites/AI_Aggressive.png");
+		aicareful.loadFromFile("../src/Graphics/CharacterSprites/AI_Careful.png");
+		aiboss.loadFromFile("../src/Graphics/CharacterSprites/AI_Boss.png");
+		airandom.loadFromFile("../src/Graphics/CharacterSprites/AI_Random.png");
+		aistunned.loadFromFile("../src/Graphics/CharacterSprites/AI_Stunned.png");
+		defense1.loadFromFile("../src/Graphics/CharacterSprites/Defense1.png");
+		defense2.loadFromFile("../src/Graphics/CharacterSprites/Defense2.png");
+		defense3.loadFromFile("../src/Graphics/CharacterSprites/Defense3.png");
+		melee1.loadFromFile("../src/Graphics/CharacterSprites/Melee1.png");
+		melee2.loadFromFile("../src/Graphics/CharacterSprites/Melee2.png");
+		melee3.loadFromFile("../src/Graphics/CharacterSprites/Melee3.png");
+		ranged1.loadFromFile("../src/Graphics/CharacterSprites/Ranged1.png");
+		ranged2.loadFromFile("../src/Graphics/CharacterSprites/Ranged2.png");
+		ranged3.loadFromFile("../src/Graphics/CharacterSprites/Ranged3.png");
+		emptyaction.loadFromFile("../src/Graphics/CharacterSprites/Empty.png");
+		enemybase.loadFromFile("../src/Graphics/CharacterSprites/Enemy.png");
+		playerbase.loadFromFile("../src/Graphics/CharacterSprites/Player.png");
+		depth1.loadFromFile("../src/Graphics/GUI_Sprites/Depth1.png");
+		depth2.loadFromFile("../src/Graphics/GUI_Sprites/Depth2.png");
+		depth3.loadFromFile("../src/Graphics/GUI_Sprites/Depth3.png");
+		depth4.loadFromFile("../src/Graphics/GUI_Sprites/Depth4.png");
+		depth5.loadFromFile("../src/Graphics/GUI_Sprites/Depth5.png");
+		depth6.loadFromFile("../src/Graphics/GUI_Sprites/Depth6.png");
+		health0.loadFromFile("../src/Graphics/GUI_Sprites/Health0.png");
+		health1.loadFromFile("../src/Graphics/GUI_Sprites/Health1.png");
+		health2.loadFromFile("../src/Graphics/GUI_Sprites/Health2.png");
+		health3.loadFromFile("../src/Graphics/GUI_Sprites/Health3.png");
+		health4.loadFromFile("../src/Graphics/GUI_Sprites/Health4.png");
+		nrzero.loadFromFile("../src/Graphics/GUI_Sprites/0.png");
+		nrone.loadFromFile("../src/Graphics/GUI_Sprites/1.png");
+		nrtwo.loadFromFile("../src/Graphics/GUI_Sprites/2.png");
+		nrthree.loadFromFile("../src/Graphics/GUI_Sprites/3.png");
+		nrfour.loadFromFile("../src/Graphics/GUI_Sprites/4.png");
+		nrfive.loadFromFile("../src/Graphics/GUI_Sprites/5.png");
+		nrsix.loadFromFile("../src/Graphics/GUI_Sprites/6.png");
+		nrseven.loadFromFile("../src/Graphics/GUI_Sprites/7.png");
+		nreight.loadFromFile("../src/Graphics/GUI_Sprites/8.png");
+		nrinf.loadFromFile("../src/Graphics/GUI_Sprites/Inf.png");
+		att.loadFromFile("../src/Graphics/GUI_Sprites/Att.png");
+		defse.loadFromFile("../src/Graphics/GUI_Sprites/Def.png");
+		coold.loadFromFile("../src/Graphics/GUI_Sprites/CD.png");
+		maxcoold.loadFromFile("../src/Graphics/GUI_Sprites/TCD.png");
+		dur.loadFromFile("../src/Graphics/GUI_Sprites/Dur.png");
+		nonum.loadFromFile("../src/Graphics/GUI_Sprites/NoNum.png");
+		infobtnview.loadFromFile("../src/Graphics/GUI_Sprites/infobtnview.png");
+		diminvbutton.loadFromFile("../src/Graphics/GUI_Sprites/DimInvButton.png");
+		hugeslimeinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/HugeSlimeInfo.png");
+		hugespiderinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/HugeSpiderInfo.png");
+		largeslimeinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/LargeSlimeInfo.png");
+		lichinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/LichInfo.png");
+		selectionrectangle.loadFromFile("../src/Graphics/CharacterInfoSprites/SelectionRectangle.png");
 		skeletonarcherinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/SkeletonArcherInfo.png");
-
+		skeletonarcherinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/SkeletonArcherInfo.png");
 		skeletoninfo.loadFromFile("../src/Graphics/CharacterInfoSprites/SkeletonInfo.png");
 		skeletonknightinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/SkeletonKnightInfo.png");
 		skeletonmageinfo.loadFromFile("../src/Graphics/CharacterInfoSprites/SkeletonMageInfo.png");

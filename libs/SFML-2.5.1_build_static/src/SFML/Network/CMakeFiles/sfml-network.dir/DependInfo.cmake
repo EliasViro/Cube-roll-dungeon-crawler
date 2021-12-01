ca18a7154737c78891f957f57a345d6c3fb6ad3f
Atte <aarnioa6@aalto.fi>
2021-12-01 16:36:29 +0200

    Testing new make configuration.

---
+++ libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/Ftp.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/Ftp.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/Http.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/Http.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/IpAddress.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/IpAddress.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/Packet.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/Packet.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/Socket.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/Socket.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/SocketSelector.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/SocketSelector.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/TcpListener.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/TcpListener.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/TcpSocket.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/TcpSocket.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/UdpSocket.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/UdpSocket.cpp.o"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src/SFML/Network/Unix/SocketImpl.cpp" "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/Network/CMakeFiles/sfml-network.dir/Unix/SocketImpl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/include"
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/atte/cpp_projects/DungeonCrawler/libs/SFML-2.5.1_build_static/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
